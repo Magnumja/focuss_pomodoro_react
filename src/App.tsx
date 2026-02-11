18c547eee4d5248f5a3281066f030e15a3d5f69a
Magnum Johanson de Abreu <magnumjabreuu@gmail.com>
2026-02-11 17:25:28 -0400

    Comeco dos trabalhos

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+export function App() {
+    console.log('Oi');
+
+    return (
+        <div>
+            <h1>OLa</h1>
+            <p>
+                qauqluer coisa
+            </p>
+        </div>
+    );
+
+    
+}
+
+export default App;
